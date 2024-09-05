ac93ed87361236f8517b1e3481d05e7da6412be3
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-09-05 23:00:48 +0100

    Allow retrying upload and download

--- apps/transcoder/src/config.ts
+++ apps/transcoder/src/config.ts
@@ -1,4 +1,7 @@
 export const OUTPUT_SUBMIT_METHOD = "PUT";
 export const PROGRESS_REPORT_METHOD = "PUT";
 
+export const MAX_DOWNLOAD_ATTEMPTS = 10;
+export const MAX_UPLOAD_ATTEMPTS = 10;
+
 export const ERRONEOUS_EXIT_CODE = 1;
--- apps/transcoder/src/transcoder.ts
+++ apps/transcoder/src/transcoder.ts
@@ -2,11 +2,13 @@ import fluentFfmpeg from "fluent-ffmpeg";
 import { mkdtemp, rm } from "node:fs/promises";
 import os from "node:os";
 import path from "node:path";
+import retry from "p-retry";
 
 import { EncodingFormat, EncodingParameters } from "./schema.js";
 import { assertUnreachable } from "./utils.js";
 import { logger } from "./logger.js";
 import { downloadFile, uploadFile } from "./fetch.js";
+import { MAX_DOWNLOAD_ATTEMPTS, MAX_UPLOAD_ATTEMPTS } from "./config.js";
 
 async function transcodeFile(srcFilePath: string, dstFilePath: string, encodingParameters: EncodingParameters) {
   const ffmpegCommand = fluentFfmpeg(srcFilePath);
@@ -44,11 +46,27 @@ export async function transcode(inputFileUrl: URL, outputFileUrl: URL, encodingP
   const dstFilePath = path.join(tmpDir, dstFileName);
 
   try {
-    // TODO: Should be retried.
-    await downloadFile(inputFileUrl, srcFilePath);
+    await retry(() => downloadFile(inputFileUrl, srcFilePath), {
+      onFailedAttempt(error) {
+        logger.warn(
+          { reason: error },
+          `Download attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`,
+        );
+      },
+      retries: MAX_DOWNLOAD_ATTEMPTS,
+    });
+
     await transcodeFile(srcFilePath, dstFilePath, encodingParameters);
-    // TODO: Should be retried.
-    await uploadFile(dstFilePath, outputFileUrl);
+
+    await retry(() => uploadFile(dstFilePath, outputFileUrl), {
+      onFailedAttempt(error) {
+        logger.warn(
+          { reason: error },
+          `Upload attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`,
+        );
+      },
+      retries: MAX_UPLOAD_ATTEMPTS,
+    });
   } finally {
     await rm(tmpDir, { recursive: true, force: true });
   }
