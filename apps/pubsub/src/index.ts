8ce76c2869781e8d307e9abdf3c0bcc6a7fabd21
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-09-14 23:49:43 +0100

    Refactoring of ws/http handlers

--- apps/pubsub/src/index.ts
+++ apps/pubsub/src/index.ts
@@ -1,52 +1,21 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { StatusCodes } from "http-status-codes";
-
-import { Server } from "socket.io";
 import { Redis } from "ioredis";
-import { createAdapter } from "@socket.io/redis-adapter";
 
 import { config } from "./config.js";
 import { logger } from "./logger.js";
+import { createApp } from "./http.js";
+import { createWebSocket } from "./ws.js";
 
-const pubClient = Redis.createClient();
+const pubClient = Redis.createClient(config.redisUrl);
 const subClient = pubClient.duplicate();
 
 pubClient.on("error", (err) => logger.error(err));
 subClient.on("error", (err) => logger.error(err));
 
-const io = new Server({
-  path: "/api/ws/",
-  transports: ["websocket"],
-  adapter: createAdapter(pubClient, subClient),
-});
-
-io.use((socket, next) => {
-  const sessionId = socket.handshake.query.sessionId;
-
-  if (sessionId) {
-    socket.join(sessionId);
-    next();
-  } else {
-    next(new Error("Query parameter sessionId is required"));
-  }
-});
-
-const app = express();
-
-app.use(bodyParser.json());
-
-app.post("/api/sessions/:sessionId/events", async (req, res) => {
-  const { sessionId } = req.params;
-  const body = req.body;
-
-  io.in(sessionId).emit("event", body);
-
-  res.status(StatusCodes.ACCEPTED).end();
-});
+const ws = createWebSocket(pubClient, subClient);
+const app = createApp(ws);
 
 const server = app.listen(config.port, () => {
   logger.info(`Server is running on port ${config.port}`);
 });
 
-io.attach(server);
+ws.attach(server);
