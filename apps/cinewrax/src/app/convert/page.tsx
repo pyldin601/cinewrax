ac639382c2dbeb4d4d89ab095e9f5da34b3a4cbb
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-09-26 19:25:14 +0100

    Add micro steps to Convert page

--- apps/cinewrax/src/app/convert/page.tsx
+++ apps/cinewrax/src/app/convert/page.tsx
@@ -5,5 +5,25 @@ import { useSessionId } from "../../hooks/useSessionId";
 export default function Convert() {
   const sessionId = useSessionId();
 
-  return <div>Session: {sessionId}</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Choose file</h2>
+          <input type={"file"} />
+        </div>
+        <div>
+          <h2>Choose format</h2>
+          <div>Formats...</div>
+        </div>
+        <div>
+          <h2>Choose bitrate</h2>
+          <div>Bitrate...</div>
+        </div>
+        <div>
+          <button>Convert</button>
+        </div>
+      </div>
+    </div>
+  );
 }
